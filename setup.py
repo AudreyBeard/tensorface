1fb534f7eee5a4e44e15f088bd57a55978ba516e
Audrey Beard <audrey.s.beard@gmail.com>
2019-11-10 12:02:28 -0500

    first real commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tensorface',
+    version='0.0.0',
+    description='Tensorboard interFace',
+    long_description=long_description,
+    author='Audrey Beard',
+    author_email='audrey.s.beard@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'tensorboard>=2.0.0',
+    ],
+    url='https://gitlab.com/AudreyBeard/tensorface',
+        changelog={
+            '0.0.0': 'first draft, supports grabbing all scalar values for a given run and for all runs',
+    }
+)
